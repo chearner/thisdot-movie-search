3c370881fecad52116f95c35f94e09c1c7e5487d
Chris Hearn <chearner@gmail.com>
2024-07-22 21:52:07 -0400

    change animation

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = ({ isOpen, onClose, title, poster, rating, children }) => {
   return (
     <div className='fixed inset-5 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none'>
       <div className='fixed inset-0 bg-black opacity-50' onClick={onClose}></div>
-      <div style={{ '--image-url': `url(${poster})` }} className={`relative w-auto max-w-3xl bg-no-repeat bg-ken-burns pan-topleft rounded-xl z-50 h-full min-h-full bg-[image:var(--image-url)]`}>
+      <div style={{ '--image-url': `url(${poster})` }} className={`relative w-auto max-w-3xl bg-no-repeat bg-cover bg-ken-burns rounded-xl z-50 h-full min-h-full bg-[image:var(--image-url)]`}>
         <div style={{ '--color-1': `#000`, '--color-2': '#fff' }} className={`relative flex flex-col w-full rounded-xl my-auto min-h-full outline-none focus:outline-none p-[50px] bg-gradient-to-b from-black`}>
           <div className='flex pb-2'>
             <h3 className='text-3xl font-bold text-white'>
